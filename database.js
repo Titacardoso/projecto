b56457b635797750f7eef48621fea3f1d14a4c25
Titacardoso <azevedotitacardoso@gmail.com>
2026-06-12 12:14:08 +0100

    Actualizacao.1

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    port: Number(process.env.DATABASE_PORT),
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME
--- server.js
+++ server.js
@@ -62,7 +62,60 @@ app.post("/api/frota", async (req, res) => {
 
 });
 
+app.put("/api/frota/:id", async (req, res) => {
 
+    const id = Number(req.params.id);
+
+    const { Matricula, Marca, Modelo, Ano, VIN, Tipo } = req.body;
+
+    const [viatura] = await pool.execute(
+        "SELECT * FROM viaturas WHERE id = ?",
+        [id]
+    );
+
+    if (viatura.length === 0) {
+        return res.status(404).json({
+            mensagem: "Viatura não encontrada"
+        });
+    }
+
+    await pool.execute(
+        `UPDATE viaturas
+        SET Matricula = ?, Marca = ?, Modelo = ?, Ano = ?, VIN = ?, Tipo = ?
+        WHERE id = ?`,
+        [Matricula, Marca, Modelo, Ano, VIN, Tipo, id]
+    );
+
+    res.status(200).json({
+        mensagem: "Viatura atualizada com sucesso"
+    });
+
+});
+app.delete("/api/frota/:id", async (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const [viatura] = await pool.execute(
+        "SELECT * FROM viaturas WHERE id = ?",
+        [id]
+    );
+
+    if (viatura.length === 0) {
+        return res.status(404).json({
+            mensagem: "Viatura não encontrada"
+        });
+    }
+
+    await pool.execute(
+        "DELETE FROM viaturas WHERE id = ?",
+        [id]
+    );
+
+    res.status(200).json({
+        mensagem: "Viatura eliminada com sucesso"
+    });
+
+});
 app.listen(PORT, () => {
     console.log("Servidor a correr na porta 3000");
 });
